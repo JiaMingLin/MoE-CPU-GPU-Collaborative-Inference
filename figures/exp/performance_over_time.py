30f0f70c1d4a905f98c86c65fda036b9197c3962
Samuel Huang <enminghuang21119@gmail.com>
2024-10-21 15:51:44 +0800

    Add scripts to draw plots

--- figures/exp/performance_over_time.py
+++ figures/exp/performance_over_time.py
@@ -15,6 +15,7 @@ def make_output():
 def get_cpu_or_gpu_only_time(path: str, omp_num_threads: int = -1):
     # Initialize lists to store temperatures and CPU frequencies
     token_time = []
+    temp = []
     if omp_num_threads == -1:
         log = os.path.join(path, 'GPU_Offload_log', 'token_gen_time.csv')
     else:
@@ -29,10 +30,9 @@ def get_cpu_or_gpu_only_time(path: str, omp_num_threads: int = -1):
             for row in reader:
                 token_time.append(float(
                     row[1]))  # Assuming time is in the second column
+                temp.append(float(row[5]))
     # Create a DataFrame from the lists
-    df = pd.DataFrame({
-        'Generation time': token_time,
-    })
+    df = pd.DataFrame({'Generation time': token_time, 'Temperature': temp})
     return df
 
 
@@ -45,29 +45,36 @@ def get_collab_token_gen_throughput(path: str, omp_num_threads: int):
     # Read data from log files
     df = {}
     token_time = []
+    temp = []
+    best_name = ''
 
     for file in glob.glob(log_dir):
         omp_number = int(
             os.path.basename(os.path.dirname(file)).split('_')[-1])
         data = []
+        temp_data = []
         with open(file, 'r') as f:
             reader = csv.reader(f)
             headers = next(reader)  # Skip the header row
             for row in reader:
-                data.append(float(
-                    row[1]))  # Assuming time is in the second column
+                # Assuming time is in the second column
+                data.append(float(row[1]))
+                temp_data.append(float(row[5]))
             if len(token_time) == 0:
                 token_time = data
+                best_name = file
+                temp = temp_data
             else:
                 avg_data = sum(data) / len(data)
                 avg_token_time = sum(token_time) / len(token_time)
                 if avg_data < avg_token_time:
                     token_time = data
+                    best_name = file
+                    temp = temp_data
 
-        # Get the nth index from the DataFrame
-    df = pd.DataFrame({
-        'Generation time': token_time,
-    })
+    # Get the nth index from the DataFrame
+    print(f'Best cache policy: {best_name}')
+    df = pd.DataFrame({'Generation time': token_time, 'Temperature': temp})
     return df
 
 
@@ -89,28 +96,53 @@ if __name__ == '__main__':
     LIMIT_ITEMS = 1800  # Limit the number of items to plot
     df_cpu['Generation time'] = df_cpu['Generation time'].rolling(
         window=WINDOW_SIZE).mean()[:LIMIT_ITEMS] * 1e3
+    df_cpu['Temperature'] = df_cpu['Temperature'].rolling(
+        window=WINDOW_SIZE).mean()[:LIMIT_ITEMS]
     df_gpu['Generation time'] = df_gpu['Generation time'].rolling(
         window=WINDOW_SIZE).mean()[:LIMIT_ITEMS] * 1e3
+    df_collab['Temperature'] = df_collab['Temperature'].rolling(
+        window=WINDOW_SIZE).mean()[:LIMIT_ITEMS]
     df_collab['Generation time'] = df_collab['Generation time'].rolling(
         window=WINDOW_SIZE).mean()[:LIMIT_ITEMS] * 1e3
+    df_collab['Temperature'] = df_collab['Temperature'].rolling(
+        window=WINDOW_SIZE).mean()[:LIMIT_ITEMS]
 
-    plt.figure(figsize=(10, 3))
+    fig, ax1 = plt.subplots(figsize=(10, 3))
 
-    plt.plot(df_gpu['Generation time'], label='GPU Offloading')
-    plt.plot(df_cpu['Generation time'], label='CPU Only')
-    plt.plot(df_collab['Generation time'], label='CPU-GPU Collab')
-    # put the label at the left top
+    ax1.plot(df_gpu['Generation time'],
+             label='GPU Offloading',
+             color='tab:blue')
+    ax1.plot(df_cpu['Generation time'], label='CPU Only', color='tab:orange')
+    ax1.plot(df_collab['Generation time'],
+             label='CPU-GPU Collab',
+             color='tab:green')
+    ax1.set_ylim(200, 1000)
+    ax1.set_xlabel('Tokens')
+    ax1.set_ylabel('Generation Time (ms.)', color='tab:blue')
+    ax1.tick_params(axis='y', labelcolor='tab:blue')
+    # ax1.set_title(
+    #     f'Token Generation Time Comparison Over Time (# of threads: {omp})')
+    # ax1.legend(loc=(0.01, 0.6))
+    # ax1.legend(loc="upper")
+    ax1.legend(bbox_to_anchor=(-0.07, 0.98, 1, 0.2), loc="upper left", ncol=3)
+    ax1.grid(True)
 
-    plt.ylim(200, 1000)
-    plt.xlabel('Tokens')
-    plt.ylabel('Generation Time (ms.)')
-    plt.title(
-        f'Token Generation Time Comparison Over Time (# of threads: {omp})')
-    plt.legend(loc=(0.01, 0.6))
-    plt.grid(True)
+    ax2 = ax1.twinx()
+    ax2.plot(df_cpu['Temperature'],
+             label='CPU Only Temp.',
+             color='#b84f23',
+             linestyle='--')
+    ax2.plot(df_collab['Temperature'],
+             label='CPU-GPU Collab Temp.',
+             color='#05754e',
+             linestyle='--')
+    ax2.set_ylabel('Temperature (°C)', color='tab:red')
+    ax2.tick_params(axis='y', labelcolor='tab:red')
+    ax2.legend(loc="upper center")
+    ax2.legend(bbox_to_anchor=(0.05, 0.98, 1, 0.2), loc="upper right", ncol=2)
 
     output_dir = make_output()
-    plt.savefig(os.path.join(output_dir, f'gen_time_history_OMP_{omp}.pdf'),
+    fig.savefig(os.path.join(output_dir, f'gen_time_history_OMP_{omp}.pdf'),
                 bbox_inches='tight',
                 format='pdf')
     plt.show()
--- figures/exp/temperature.py
+++ figures/exp/temperature.py
@@ -5,16 +5,18 @@ import argparse
 import pandas as pd
 import matplotlib.pyplot as plt
 
+
 def make_output():
     output_dir = 'output'
     os.makedirs(output_dir, exist_ok=True)
     return output_dir
 
-def get_cpu_tmp_freq(path: str, omp_num_threads: int):
+
+def get_cpu_tmp_freq(path: str):
     # Initialize lists to store temperatures and CPU frequencies
     temperatures = []
     cpu_frequencies = []
-    log = os.path.join(path, f'OMP_{omp_num_threads}', 'cpu_freq_avg.csv')
+    log = os.path.join(path, 'cpu_freq_avg.csv')
 
     # Read files from the specified directory
     for file in glob.glob(log):
@@ -22,8 +24,10 @@ def get_cpu_tmp_freq(path: str, omp_num_threads: int):
             reader = csv.reader(f)
             headers = next(reader)  # Skip the header row
             for row in reader:
-                temperatures.append(float(row[1]))  # Assuming temperature is in the first column
-                cpu_frequencies.append(float(row[2]))  # Assuming CPU frequency is in the second column
+                temperatures.append(float(
+                    row[1]))  # Assuming temperature is in the first column
+                cpu_frequencies.append(float(
+                    row[2]))  # Assuming CPU frequency is in the second column
     # Create a DataFrame from the lists
     df = pd.DataFrame({
         'Temperature': temperatures,
@@ -31,28 +35,40 @@ def get_cpu_tmp_freq(path: str, omp_num_threads: int):
     })
     return df
 
+
 if __name__ == '__main__':
     # argv input: --omp
     # parse argv
     parser = argparse.ArgumentParser()
     parser.add_argument('--omp', type=int, help='Number of OpenMP threads')
+    # parser.add_argument('--collab-file',
+    #                     type=str,
+    #                     help='Best collab configuration to be compared with')
     args = parser.parse_args()
 
     omp = args.omp
-    df = get_cpu_tmp_freq('../../logs/OMP_logs', omp)
-    print(df)
+    WINDOW_SIZE = 5  # You can adjust the window size as needed
+    df1 = get_cpu_tmp_freq(os.path.join('../../logs/OMP_logs', f'OMP_{omp}'))
+    df1 = df1.rolling(window=WINDOW_SIZE).mean()
+    # df2 = get_cpu_tmp_freq(args.collab_file)
+    # df2 = df2.rolling(window=WINDOW_SIZE).mean()
     # Plot the data
     fig, ax1 = plt.subplots(figsize=(10, 5))
 
     # Plot temperature on the left y-axis
-    ax1.plot(df['Temperature'], color='tab:red', label='Temperature')
+    ax1.plot(df1['Temperature'],
+             color='tab:red',
+             label='Temperature (CPU only)')
+    # ax1.plot(df2['Temperature'],
+    #          color='#7b1818',
+    #          label='Temperature (CPU-GPU Collab)')
     ax1.set_xlabel('Time (sec.)')
     ax1.set_ylabel('Temperature', color='tab:red')
     ax1.tick_params(axis='y', labelcolor='tab:red')
 
     # Create a second y-axis for CPU frequency
     ax2 = ax1.twinx()
-    ax2.plot(df['CPU Frequency'], color='tab:blue', label='CPU Frequency')
+    ax2.plot(df1['CPU Frequency'], color='tab:blue', label='CPU Frequency')
     ax2.set_ylabel('CPU Frequency', color='tab:blue')
     ax2.tick_params(axis='y', labelcolor='tab:blue')
 
@@ -64,5 +80,7 @@ if __name__ == '__main__':
     fig.tight_layout()  # Adjust layout to make room for both y-axes
 
     output_dir = make_output()
-    plt.savefig(os.path.join(output_dir, f'temperature_OMP_{omp}.pdf'), format='pdf', bbox_inches='tight')
+    plt.savefig(os.path.join(output_dir, f'temperature_OMP_{omp}.pdf'),
+                format='pdf',
+                bbox_inches='tight')
     plt.show()
